9f586d2b3163ab291f15aeb49043af3ab25b0841
Anber <shuangyan_m@hotmail.com>
2020-06-29 20:00:30 +0800

    chore: 使用谷歌地图底图

--- src/app/area-map/area-map.component.ts
+++ src/app/area-map/area-map.component.ts
@@ -3,6 +3,8 @@ import { DrawControl, DrawPolygon } from '@antv/l7-draw';
 import { Mapbox } from '@antv/l7-maps';
 import { Scene } from '@antv/l7';
 
+declare const mapboxgl: any;
+
 @Component({
   selector: 'fd-area-map',
   templateUrl: './area-map.component.html',
@@ -101,13 +103,33 @@ export class AreaMapComponent implements OnInit {
         },
       ],
     };
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiYW5iZXJtIiwiYSI6ImNrYnJqNXVnaDJ3ejMyb214a2U4MXlkazkifQ.nsXT5HbJUuhQ6ECvR9i2Sw';
+    const map = new mapboxgl.Map({
+      container: this.areaMap.nativeElement, // container id
+      pitch: 0,
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [118.79804, 36.505044],
+      zoom: 15,
+    });
+    map.on('load', () => {
+      map.addSource('google.tile', {
+        type: 'raster',
+        tiles: [
+          'https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',
+        ],
+        tileSize: 256,
+      });
+      map.addLayer({
+        id: 'google',
+        type: 'raster',
+        source: 'google.tile',
+      });
+    });
     const scene = new Scene({
       id: this.areaMap.nativeElement,
       map: new Mapbox({
-        pitch: 0,
-        style: 'mapbox://styles/mapbox/satellite-streets-v11',
-        center: [118.79804, 36.505044],
-        zoom: 15,
+        mapInstance: map,
       }),
       logoVisible: false,
     });
--- src/app/draw-map/draw-map.component.ts
+++ src/app/draw-map/draw-map.component.ts
@@ -3,6 +3,7 @@ import { DrawControl } from '@antv/l7-draw';
 import { Mapbox } from '@antv/l7-maps';
 import { Scene } from '@antv/l7';
 declare const L7: any;
+declare const mapboxgl: any;
 
 @Component({
   selector: 'fd-draw-map',
@@ -19,13 +20,33 @@ export class DrawMapComponent implements OnInit {
   }
 
   mapRender() {
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiYW5iZXJtIiwiYSI6ImNrYnJqNXVnaDJ3ejMyb214a2U4MXlkazkifQ.nsXT5HbJUuhQ6ECvR9i2Sw';
+    const map = new mapboxgl.Map({
+      container: this.drawMap.nativeElement, // container id
+      pitch: 0,
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [118.79804, 36.505044],
+      zoom: 15,
+    });
+    map.on('load', () => {
+      map.addSource('google.tile', {
+        type: 'raster',
+        tiles: [
+          'https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}',
+        ],
+        tileSize: 256,
+      });
+      map.addLayer({
+        id: 'google',
+        type: 'raster',
+        source: 'google.tile',
+      });
+    });
     const scene = new Scene({
       id: this.drawMap.nativeElement,
       map: new Mapbox({
-        pitch: 0,
-        style: 'mapbox://styles/mapbox/satellite-streets-v11',
-        center: [118.79804, 36.505044],
-        zoom: 15,
+        mapInstance: map,
       }),
       logoVisible: false,
     });
